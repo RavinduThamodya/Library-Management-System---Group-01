36250e73ab4e4bf3916c87dc742f7e6be1cb7535
Imandee Gunaratne <sandanithaimandeegunaratne@gmail.com>
2025-04-23 21:56:55 +0530

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+]
