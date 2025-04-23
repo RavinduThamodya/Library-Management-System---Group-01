9680e27593ba96cad2b5885953ca5a974960acb3
dasundananjana <dasundananjana2021@gmail.com>
2025-04-23 14:12:51 +0530

    Update urls.py

---
+++ books/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.landing_page, name='landing'),
+     path('home/', views.home, name='home'), 
+    path('reserve/<int:book_id>/', views.reserve_book, name='reserve_book'),
+    path('cancel/<int:book_id>/', views.cancel_reservation, name='cancel_reservation'),
+    path('my-reservations/', views.my_reservations, name='my_reservations'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
